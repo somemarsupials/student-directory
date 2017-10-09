32bbfe395b14b5208d708cfb39b69eca9a4e9160
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-10-09 16:50:13 +0100

    changed source of students from pre-defined array to input function

--- directory.rb
+++ directory.rb
@@ -1,32 +1,39 @@
-# put students into an array
-students = [
-   {name: "Dr. Hannibal Lecter", cohort: :november},
-   {name: "Darth Vader", cohort: :november},
-   {name: "Nurse Ratched", cohort: :november},
-   {name: "Michael Corleone", cohort: :november},
-   {name: "Alex DeLarge", cohort: :november},
-   {name: "The Wicked Witch of the West", cohort: :november},
-   {name: "Terminator", cohort: :november},
-   {name: "Freddy Krueger", cohort: :november},
-   {name: "The Joker", cohort: :november},
-   {name: "Joffrey Baratheon", cohort: :november},
-   {name: "Norman Bates", cohort: :november},
-]
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+	students = []
+	# get first name
+	name = gets.chomp
+	# while name is not empty, repeat this code
+	while !name.empty?
+		# add new hash to the array
+		students << {name: name, cohort: :november}
+		puts "Now we have #{students.count} students"
+		# get another name
+		name = gets.chomp
+	end
+	students
+end
+
 
 def print_header
 	puts "The students of Villains' Academy"
 	puts "-" * 13
 end
 
+
 def print_array(arr)
 	arr.each { |s| puts "#{s[:name]} (#{s[:cohort]} cohort)" }
 end
+
 
 def print_footer(arr)
 	puts "Overall, we have #{arr.length} great students"
 end
 
 # print introduction
+students = input_students
 print_header
 print_array(students)
 print_footer(students)
