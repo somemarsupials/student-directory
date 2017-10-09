1a7b1d663a9137d55e61a5d3e966fa8274b51b76
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-10-09 16:29:09 +0100

    added cohorts and changed print statement to include them

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 # put students into an array
 students = [
-   "Dr. Hannibal Lecter",
-   "Darth Vader",
-   "Nurse Ratched",
-   "Michael Corleone",
-   "Alex DeLarge",
-   "The Wicked Witch of the West",
-   "Terminator",
-   "Freddy Krueger",
-   "The Joker",
-   "Joffrey Baratheon",
-   "Norman Bates",
-	 ]
+   {name: "Dr. Hannibal Lecter", cohort: :november},
+   {name: "Darth Vader", cohort: :november},
+   {name: "Nurse Ratched", cohort: :november},
+   {name: "Michael Corleone", cohort: :november},
+   {name: "Alex DeLarge", cohort: :november},
+   {name: "The Wicked Witch of the West", cohort: :november},
+   {name: "Terminator", cohort: :november},
+   {name: "Freddy Krueger", cohort: :november},
+   {name: "The Joker", cohort: :november},
+   {name: "Joffrey Baratheon", cohort: :november},
+   {name: "Norman Bates", cohort: :november},
+]
 
 def print_header
 	puts "The students of Villains' Academy"
@@ -19,7 +19,7 @@ def print_header
 end
 
 def print_array(arr)
-	arr.each { |s| puts s }
+	arr.each { |s| puts "#{s[:name]} (#{s[:cohort]} cohort)" }
 end
 
 def print_footer(arr)
